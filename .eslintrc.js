7f2e2ebf2145311b8d9ac3ccaa69f718ca042198
Caposto <christiand.apostol@outlook.com>
2023-03-14 13:10:38 -0400

    added eslint-plugin-cypress

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:prettier/recommended",
+    "plugin:cypress/recommended",
   ],
   overrides: [],
   parserOptions: {
