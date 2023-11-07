c80d31c9900a51c9078af045c4fd9fbd72b6b6d0
Terrence Zhang <96602253+ZhangTerrence@users.noreply.github.com>
2023-11-07 00:11:51 -0500

    feat: added ProjectCard component (#44)

--- src/_components/ProjectCard.jsx
+++ src/_components/ProjectCard.jsx
@@ -1,5 +1,29 @@
-import React from "https://esm.sh/react"
+import React from "https://esm.sh/react";
 
-export default function ProjectCard({ name_organization, image_url, description, redirect_url }) {
-    return <div></div>
+export default function ProjectCard({
+  name_organization,
+  image_url,
+  description,
+  redirect_url,
+}) {
+  return (
+    <figure className="flex flex-col w-fit p-4 max-sm:w-full">
+      <div
+        className={`bg-[url(${image_url})] bg-cover bg-center w-full h-80 rounded-xl relative`}
+      >
+        <div className="absolute top-0 right-0 m-4 bg-black/60 backdrop-blur-4xl text-white rounded-lg px-6 py-1 max-sm:left-0 max-sm:right-auto">
+          Placeholder info
+        </div>
+      </div>
+      <figcaption>
+        <h1 className="font-bold my-2 text-2xl">{name_organization}</h1>
+        <p className="max-w-sm line-clamp-3 max-sm:max-w-full">{description}</p>
+      </figcaption>
+      <div className="mt-2">
+        <a className="text-blue-500 font-semibold text-lg" href={redirect_url}>
+          Read More
+        </a>
+      </div>
+    </figure>
+  );
 }
