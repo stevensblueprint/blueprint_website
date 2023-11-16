c2e4bc3a71438d187a1a1d1d09f5a92c1db3c67b
Terrence Zhang <96602253+ZhangTerrence@users.noreply.github.com>
2023-11-16 16:48:47 -0500

    feat: added Home template (#22)

--- src/_components/ProjectCard.jsx
+++ src/_components/ProjectCard.jsx
@@ -7,11 +7,12 @@ export default function ProjectCard({
   redirect_url,
 }) {
   return (
-    <figure className="flex flex-col w-fit max-sm:w-full">
+    <figure className="flex flex-col w-full max-sm:w-full">
       <div
         className={`bg-[url(${image_url})] bg-contain bg-no-repeat bg-center h-40 rounded-xl relative`}
       >
         <div className="absolute top-0 right-0 m-4 bg-black/60 backdrop-blur-4xl text-white rounded-lg px-6 py-1 max-sm:left-0 max-sm:right-auto">
+          {/* This may not be needed. If it is, there should probably be an additional prop. */}
           Placeholder info
         </div>
       </div>
