5f3b703d1c58e60fb9a5478e82f1a3add682a5b0
miguel-merlin <mmerlin@stevens.edu>
2024-02-05 13:40:25 -0500

    fix: Changed Member Card Component to fix resizing of avatars

--- src/_components/MemberCard.jsx
+++ src/_components/MemberCard.jsx
@@ -2,14 +2,16 @@ import React from "https://esm.sh/react";
 
 export default function MemberCard({ name, role, image_url }) {
   return (
-    <figure className="flex flex-col">
+    <figure className="flex flex-col items-center">
       <img
-        className="rounded-md mb-4 w-40 basis-full max-sm:w-32"
+        className="rounded-md mb-4 object-cover object-center w-32 h-32 max-sm:w-32 max-sm:h-32"
         src={image_url}
         alt="member image"
       />
-      <div className="font-semibold">{name}</div>
-      <div>{role}</div>
+      <figcaption className="text-center">
+        <div className="font-semibold">{name}</div>
+        <div>{role}</div>
+      </figcaption>
     </figure>
   );
 }
