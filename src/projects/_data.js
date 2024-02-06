1d67fb45580ca8e46a767a8fa89eab5ab2c06d9e
Caposto <christiand.apostol@outlook.com>
2024-02-05 23:44:05 -0500

    removed unecessary regex

--- src/projects/_data.js
+++ src/projects/_data.js
@@ -1,5 +1,5 @@
 export function url(page) {
   return (
-    page.src.path.replace(/[0-9]{4}-[0-9]{2}-[0-9]{2}_/, "") + ".html" // this seems to be necessary even with the new get date from folders functionality
+    page.src.path + ".html" // this seems to be necessary even with the new get date from folders functionality
   );
 }
