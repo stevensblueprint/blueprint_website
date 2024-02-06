ecf9ac5f5fea49c9190b20ff646ae261f3104c40
ngkatie <kng7@stevens.edu>
2024-02-06 11:48:42 -0500

    Draft for Apply

--- src/_includes/_layouts/Apply.jsx
+++ src/_includes/_layouts/Apply.jsx
@@ -6,6 +6,35 @@ export default ({ comp }) => (
     </head>
     <body>
       <>
+        <comp.Navbar />
+        <section className="grid place-content-center">
+          <div className="flex flex-col items-center p-20 max-lg:px-12 max-md:w-full max-md:px-4">
+            <div className="w-full flex flex-wrap justify-evenly max-md:flex-nowrap max-md:flex-col">
+              
+              <div className="basis-1/3 flex flex-col items-center m-4 p-4 max-md:items-start max-md:p-0">
+                <h2 className="font-bold text-xl text-primary mb-2">
+                  For Students
+                </h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim adminim veniam, quis nostrud exercitation ullamco
+                  laboris nisi utaliquip ex ea commodo consequat.
+                </p>
+              </div>
+              <div className="basis-1/3 flex flex-col items-center m-2 p-2 max-md:items-start max-md:p-0">
+                <h2 className="font-bold text-xl text-primary mb-2">
+                  For Non-Profits
+                </h2>
+                <p>
+                  Stevens Blueprint believes in the power of technology to drive social good, and with your mission always
+                  top-of-mind, we are committed to delivering solutions built uniquely for you.
+                </p>
+              </div>
+              
+            </div>
+          </div>
+        </section>
         <comp.Footer />
       </>
     </body>
---
+++ src/_includes/_layouts/ForStudents.jsx
@@ -0,0 +1,63 @@
+export default ({ comp }) => (
+    <html>
+      <head>
+        <link rel="stylesheet" href="/styles.css" />
+        <title> Apply </title>
+      </head>
+      <body>
+        <>
+          <comp.Navbar />
+          <section className="grid place-content-center">
+            <div className="flex flex-col items-center p-20 max-lg:px-12 max-md:w-full max-md:px-4">
+              <div className="w-full flex flex-wrap justify-evenly max-md:flex-nowrap max-md:flex-col">
+                <ol class="flex flex-col p-10 md:flex-row md:divide-x md:divide-charcoal-500 space-y-5 md:space-y-0">
+                  <li class="flex-1 flex flex-col p-6 space-y-4 md:pr-6">
+                    <div class="relative w-16 h-16 mb-3">
+                      <h1 class="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 z-10 row-start-1 text-blue text-[48px] font-medium">1</h1>
+                    </div>
+                    <h2 className="font-bold text-xl text-primary mb-2">
+                      Written Application
+                    </h2>
+                    <p>
+                      Attend our events and info sessions to learn more about our application cycle! In your written application, tell us about yourself 
+                      and why you're interested in joining Blueprint. We anonymize your applications in this first stage, so please take care to be detailed 
+                      so we have the most holistic perspective of you. Applications open Feb 26, 2024 12:00 AM and close Mar 05, 2024 11:59 PM.
+                    </p>
+                  </li>
+  
+                  <li class="flex-1 flex flex-col p-6 space-y-4 md:pr-6">
+                    <div class="relative w-16 h-16 mb-3">
+                      <h1 class="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 z-10 row-start-1 text-blue text-[48px] font-medium">2</h1>
+                    </div>
+                    <h2 className="font-bold text-xl text-primary mb-2">
+                      Blueprint Games
+                    </h2>
+                    <p>
+                      This semester, Blueprint Games will be hosted on Mar 20, 2024 3:00 PM (invites will be sent by Mar 08). The event is a team-based 
+                      activity that simulates the product design and implementation process for a hypothetical client.
+                    </p>
+                  </li>
+  
+                  <li class="flex-1 flex flex-col p-6 space-y-4 md:pr-6">
+                    <div class="relative w-16 h-16 mb-3">
+                      <h1 class="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 z-10 row-start-1 text-blue text-[48px] font-medium">3</h1>
+                    </div>
+                    <h2 className="font-bold text-xl text-primary mb-2">
+                      Coffee Chat with Us
+                    </h2>
+                    <p>
+                      Let's talk! Invites will be sent out by Mar 24 to schedule a time to connect with a member of our recruitment team.
+                      During these chats, you'll have an opportunity to share more about how your values connect to Blueprint, and our team members
+                      will also be available to answer any of your questions. Final decisions will be released by Apr 05!
+                    </p>
+                  </li>
+                </ol>
+              </div>
+            </div>
+          </section>
+          <comp.Footer />
+        </>
+      </body>
+    </html>
+  );
+  
