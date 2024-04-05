0d04742b129b3d28d1da0557b79be154619e50bc
Eric Zhang <ericzhang200@gmail.com>
2024-04-05 03:35:39 -0400

    Fixed meta elements being in body. they are where they should be

--- src/_components/OpenGraphCommon.jsx
+++ src/_components/OpenGraphCommon.jsx
@@ -2,14 +2,12 @@ import React from "https://esm.sh/react";
 
 export default function OpenGraphCommon() {
   return (
-    <html prefix="og: https://ogp.me/ns#">
-      <head>
-        <meta property="og:type" content="website" />
-        <meta
-          property="og:image"
-          content="https://sitblueprint.com/assets/logos/logo.png"
-        />
-      </head>
-    </html>
+    <head>
+      <meta property="og:type" content="website" />
+      <meta
+        property="og:image"
+        content="https://sitblueprint.com/assets/logos/logo.png"
+      />
+    </head>
   );
 }
