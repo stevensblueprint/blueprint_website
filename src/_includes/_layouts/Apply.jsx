4a6f20c910b2ea72b71a9314e4daf3c2de7760f5
jwang14@stevens.edu <jwang14@stevens.edu>
2024-07-18 16:46:39 -0400

    context change to match figma

--- src/_includes/_layouts/Apply.jsx
+++ src/_includes/_layouts/Apply.jsx
@@ -15,19 +15,20 @@ export default ({ comp }) => (
             <div className="w-full flex flex-wrap justify-evenly max-md:flex-nowrap max-md:flex-col">
               <div className="basis-1/3 flex flex-col items-center m-4 p-4 max-md:items-start max-md:p-0">
                 <h2 className="font-bold text-xl text-primary mb-2">
-                  For Students
+                  Students
                 </h2>
                 <p>
-                  At Blueprint, we are dedicated to leveraging our technical
-                  skills to drive positive social impact. Our culture is as
-                  important as our projects, and we value the relationships and
-                  learning opportunities we find in each other!
+                  Our application cycle to join projects begins at 
+                  the beginning of each semester. We also have 
+                  internal opportunities in the Tech Team, where
+                  you can work on the software we use within 
+                  the organization.
                 </p>
                 <a
                   className="text-primary font-semibold mt-2"
                   href="/apply/students"
                 >
-                  Learn More
+                  Application Details
                 </a>
               </div>
               <div className="basis-1/3 flex flex-col items-center m-2 p-2 max-md:items-start max-md:p-0">
@@ -35,16 +36,17 @@ export default ({ comp }) => (
                   For Non-Profits
                 </h2>
                 <p>
-                  We believe in the power of technology to support our
-                  communities, and with your mission always top-of-mind, we are
-                  committed to delivering web solutions built uniquely for you,
-                  completely free of charge.
+                  We’re looking for end-to-end projects that are 
+                  feasible in a one year time frame. We want to 
+                  ensure our projects are impactful and 
+                  delivered within the dedicated collaboration 
+                  period. 
                 </p>
                 <a
                   className="text-primary font-semibold mt-2"
                   href="/apply/nonprofits"
                 >
-                  Learn More
+                  Collaboration Details
                 </a>
               </div>
             </div>
