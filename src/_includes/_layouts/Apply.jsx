2649dd585bd91f189fd278a71a3ed6705892cb60
jwang14@stevens.edu <jwang14@stevens.edu>
2024-07-20 23:46:27 -0400

    Matched the page with the figma

--- src/_includes/_layouts/Apply.jsx
+++ src/_includes/_layouts/Apply.jsx
@@ -12,12 +12,17 @@ export default ({ comp }) => (
         <comp.Navbar />
         <section className="grid place-content-center">
           <div className="flex flex-col items-center p-20 max-lg:px-12 max-md:w-full max-md:px-4">
-            <div className="w-full flex flex-wrap justify-evenly max-md:flex-nowrap max-md:flex-col">
-              <div className="basis-1/3 flex flex-col items-center m-4 p-4 max-md:items-start max-md:p-0">
-                <h2 className="font-bold text-xl text-primary mb-2">
+          <h1 className="text-5xl mb-8">Become part of our Blueprint community!</h1>
+          <p className="text-xl mb-16 text-center max-w-3xl">
+              Whether you’re a student at Stevens looking for a place to make an impact with design, development, or management, 
+              or a representative of a non-profit organization hoping for a technology solution, we’re the place for you!
+            </p>
+            <div className="w-full flex justify-center space-x-10 max-md:flex-col max-md:space-x-0 max-md:space-y-4">
+              <div className="basis-1/3 flex flex-col m-2 p-4 max-md:items-start max-md:p-0s border border-black rounded-lg max-w-xl">
+                <h2 className="text-2xl mb-3 mt-4">
                   Students
                 </h2>
-                <p>
+                <p className="text-[1.1rem] mb-4 flex-grow">
                   Our application cycle to join projects begins at 
                   the beginning of each semester. We also have 
                   internal opportunities in the Tech Team, where
@@ -25,17 +30,17 @@ export default ({ comp }) => (
                   the organization.
                 </p>
                 <a
-                  className="text-primary font-semibold mt-2"
+                  className="text-xl text-primary text-center font-semibold mt-1 mb-1"
                   href="/apply/students"
                 >
                   Application Details
                 </a>
               </div>
-              <div className="basis-1/3 flex flex-col items-center m-2 p-2 max-md:items-start max-md:p-0">
-                <h2 className="font-bold text-xl text-primary mb-2">
+              <div className="basis-1/3 flex flex-col m-2 p-4 max-md:items-start max-md:p-0 border border-black rounded-lg max-w-xl">
+                <h2 className="text-2xl mb-3 mt-4">
                   For Non-Profits
                 </h2>
-                <p>
+                <p className="text-[1.1rem] mb-4 flex-grow">
                   We’re looking for end-to-end projects that are 
                   feasible in a one year time frame. We want to 
                   ensure our projects are impactful and 
@@ -43,7 +48,7 @@ export default ({ comp }) => (
                   period. 
                 </p>
                 <a
-                  className="text-primary font-semibold mt-2"
+                  className="text-xl text-primary text-center font-semibold mt-1 mb-1"
                   href="/apply/nonprofits"
                 >
                   Collaboration Details
