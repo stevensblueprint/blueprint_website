bbf37f2497e151300cb4dc6486ab7c6913832bb3
EsatAdiloglu <esatadiloglu16@gmail.com>
2024-07-27 00:36:04 -0400

    7/27/2024 update

--- src/_components/InformationCard.jsx
+++ src/_components/InformationCard.jsx
@@ -8,32 +8,32 @@ export default function InformationCard({
   redirect_url_text,
 }) {
   return (
-    <figure className="flex-col flex w-full border border-black rounded-lg lg:p-5 p-8 lg:max-w-[445px]">
-      {image_url && (
-        <div className="h-40 w-full flex justify-center items-center  max-lg:h-80">
-          <img
-            src={image_url}
-            alt={`${card_header} Image`}
-            className="h-full w-full -mx-96  object-cover rounded-xl lg:max-w-[315px]"
-          />
-        </div>
-      )}
-      <figcaption>
-        <h1 className="font-semibold lg:text-2xl text-5xl my-4 lg:my-3">
-          {card_header}
-        </h1>
-        <p className="line-clamp-6 max-sm:w-full lg:text-xl text-3xl lg:leading-6">
-          {description}
-        </p>
-        <div className="lg:mt-5 mt-4 flex justify-center items-center">
-          <a
-            className="text-primary font-semibold lg:text-2xl text-4xl"
-            href={redirect_url}
-          >
-            {redirect_url_text}
-          </a>
-        </div>
-      </figcaption>
+    <figure className="flex-col flex border border-black rounded-lg lg:p-5 lg:max-w-[575px] lg:h-[450px] p-8">
+        {image_url && (
+          <div className="h-[180px] w-full flex justify-center items-center max-lg:h-80">
+            <img
+              src={image_url}
+              alt={`${card_header} Image`}
+              className="h-full w-5/6 -mx-96  object-cover rounded-xl max-lg:w-full"
+            />
+          </div>
+        )}
+        <figcaption>
+          <h1 className="font-semibold lg:text-2xl text-5xl my-4 lg:my-3">
+            {card_header}
+          </h1>
+          <p className="line-clamp-6 max-sm:w-full pb-3 lg:text-2xl text-3xl lg:leading-6">
+            {description}
+          </p>
+          <div className="lg:mt-5 mt-4 flex justify-center items-center">
+            <a
+              className="text-primary font-semibold lg:text-3xl text-4xl"
+              href={redirect_url}
+            >
+              {redirect_url_text}
+            </a>
+          </div>
+        </figcaption>
     </figure>
   );
 }
