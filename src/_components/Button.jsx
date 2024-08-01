77e754688c5d7c5f34315908eb2fbc991c138353
Shawn Aviles <shawnaviles26@gmail.com>
2024-08-01 00:00:17 -0400

    feat: create Contact Us section for homepage

--- src/_components/Button.jsx
+++ src/_components/Button.jsx
@@ -1,8 +1,14 @@
 import React from "https://esm.sh/react";
 
-export default function Button({ style, text, redirect_url }) {
+export default function Button({
+  style,
+  text,
+  redirect_url,
+  openInNewTab = false,
+}) {
+  const target = openInNewTab ? "_blank" : "";
   return (
-    <a className={style} href={redirect_url}>
+    <a className={style} href={redirect_url} target={target}>
       {text}
     </a>
   );
