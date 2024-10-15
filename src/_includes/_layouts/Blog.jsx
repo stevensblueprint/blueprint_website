2dc9aed401eaca67cf35da6c9d45374f456e6798
Alexis Emerson <alexis.g.emerson@gmail.com>
2024-10-15 18:08:37 -0400

    blog hero first draft

--- src/_includes/_layouts/Blog.jsx
+++ src/_includes/_layouts/Blog.jsx
@@ -9,122 +9,19 @@ export default ({ comp, description, posts, children }) => (
     </head>
     <body>
       <comp.Navbar />
-      <main className="max-w-6xl mx-auto p-4">
-        {/* <p className="text-lg text-gray-700 mb-8">{description}</p> */}
-        <div className="text-4xl font-bold mb-2 my-10 text-primary">
-          Stevens Blueprint Blog
-        </div>
-        <div class="grid grid-cols-2 gap-4 flex items-center mb-10">
-          <div class="col-span-1 p-4">
-            <div class="flex items-center space-x-2">
-              <img
-                src="../assets/vector/clock.svg"
-                alt="clock"
-                class="h-4 w-4 mb-1"
-              />
-              <p class="mb-1">4 May, 2024</p>
-            </div>
-            <h2 class="text-2xl font-bold mb-4">
-              Reflecting on the 2023-2024 Year
-            </h2>
-            <p className="bg-primary px-2 py-1 max-w-max my-3 rounded-md text-white lg:text-xs text-lg font-medium">
-              Yearly Recap
-            </p>
-            <p class="mb-3">
-              As the spring semester comes to an end, we reflect on our work for
-              the 2023-2024 school year. Our project teams partnered with Asian
-              American Dream and NYC Mesh...
-            </p>
-            <button
-              type="button"
-              class="text-black font-bold hover:text-black border-2 border-black hover:bg-black rounded-lg text-sm px-10 py-2.5 text-center me-2 mb-2 dark:border-black dark:text-black dark:hover:text-white dark:hover:bg-black"
-            >
-              Read More
-            </button>
-          </div>
-          <div class="col-span-1 p-4">
-            <img
-              src="../assets/events/ebm.jpeg"
-              alt="ebm"
-              class="w-full h-auto rounded-lg border-2 border-primary"
-            />
-          </div>
-        </div>
-        <div class="flex items-center mb-10">
-          <h2 class="text-2xl font-bold mr-4">Previous Posts</h2>
-
-          <div class="flex space-x-2">
-            <button
-              id="dropdownHoverButton"
-              data-dropdown-toggle="dropdownHover"
-              data-dropdown-trigger="hover"
-              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              type="button"
-            >
-              Filter by topic
-              <svg
-                class="w-2.5 h-2.5 ms-3"
-                aria-hidden="true"
-                fill="none"
-                viewBox="0 0 10 6"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="m1 1 4 4 4-4"
-                />
-              </svg>
-            </button>
-
-            <div
-              id="dropdownHover"
-              class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
-            >
-              <ul
-                class="py-2 text-sm text-gray-700 dark:text-gray-200"
-                aria-labelledby="dropdownHoverButton"
-              ></ul>
-            </div>
-
-            <button
-              id="dropdownHoverButton"
-              data-dropdown-toggle="dropdownHover"
-              data-dropdown-trigger="hover"
-              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              type="button"
-            >
-              Sort
-              <svg
-                class="w-2.5 h-2.5 ms-3"
-                aria-hidden="true"
-                fill="none"
-                viewBox="0 0 10 6"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="m1 1 4 4 4-4"
-                />
-              </svg>
-            </button>
-
-            <div
-              id="dropdownHover"
-              class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
-            >
-              <ul
-                class="py-2 text-sm text-gray-700 dark:text-gray-200"
-                aria-labelledby="dropdownHoverButton"
-              ></ul>
-            </div>
-          </div>
-        </div>
-
-        <div className="grid grid-cols-3 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-20">
+      <main className="max-w-4xl mx-auto p-4">
+        <div className="text-4xl font-bold mb-2">Stevens Blueprint Blog</div>
+        <p className="text-lg text-gray-700 mb-8">{description}</p>
+        <comp.BlogHero 
+              image_url={"../assets/events/google_event_2.jpg"}
+              card_header={"Reflecting on the 2023-2024 Year"}
+              tags={"hello"}
+              time_posted={"16 Sept 2024"}
+              description={"Join Blueprint to gain hands on experience and give back to the community."}
+              redirect_url={"https://hobokenlibrary.org/"}
+              redirect_url_text={"Read More"}
+        />
+        <div className="grid grid-cols-3 gap-4">
           {/* 'posts' is currently a static string array that matches the filenames in blogs/ folder. Needs to be dynamic. See utility/searchResults.js for more info */}
           {posts.map((post) => (
             <comp.BlogCard
--- src/blog.md
+++ src/blog.md
@@ -1,40 +1,22 @@
 ---
 title: Stevens Blueprint
-layout: _layouts/Placeholder.jsx
-headline: Under Construction!
-description: This page is being worked on, please check back in soon.
----
-
-<!-- ---
-UNCOMMENT TO SHOW MOCK BLOG POSTS
-
-title: Stevens Blueprint
 layout: _layouts/Blog.jsx
 headline: Stevens Blueprint Blog
 description: Keep up with our student run organization's latest accomplishments and mishaps.
 posts:
-  - name_post: example1
-    image: "/assets/events/google_event.jpg"
-    date: "10, May 2024"
-    tag: "Project"
-  - name_post: example2
-    image: "/assets/events/google_event_1.jpg"
-    date: "10, May 2024"
-    tag: "Project"
-  - name_post: example3
-    image: "/assets/events/google_event_2.jpg"
-    date: "10, May 2024"
-    tag: "Project"
-  - name_post: example4
-    image: "/assets/events/google_event.jpg"
-    date: "10, May 2024"
-    tag: "Project"
-  - name_post: example5
-    image: "/assets/events/google_event_1.jpg"
-    date: "10, May 2024"
-    tag: "Project"
-  - name_post: example6
-    image: "/assets/events/google_event_2.jpg"
-    date: "10, May 2024"
-    tag: "Project"
---- -->
+  - example1
+  - example2
+  - example3
+---
+
+
+<!-- title: Stevens Blueprint
+layout: _layouts/Placeholder.jsx
+headline: Under Construction!
+description: This page is being worked on, please check back in soon.
+ -->
+Blog page to learn more about Stevens Blueprint and what we do
+
+## Section
+
+Learn more about us! -->
