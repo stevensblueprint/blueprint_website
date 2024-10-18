09226a83197f6b09c18586350154d81bdd039c81
Alexis Emerson <alexis.g.emerson@gmail.com>
2024-10-18 17:23:26 -0400

    All components but not completely formatted, truncating text issue

--- src/_includes/_layouts/Blog.jsx
+++ src/_includes/_layouts/Blog.jsx
@@ -10,15 +10,13 @@ export default ({ comp, description, posts, children }) => (
     <body>
       <comp.Navbar />
       <main className="max-w-4xl mx-auto p-4">
-        <div className="text-4xl font-bold mb-2">Stevens Blueprint Blog</div>
-        <p className="text-lg text-gray-700 mb-8">{description}</p>
         <comp.BlogHero 
               image_url={"../assets/events/google_event_2.jpg"}
               card_header={"Reflecting on the 2023-2024 Year"}
-              tags={"hello"}
+              tags={["hello"]}
               time_posted={"16 Sept 2024"}
-              description={"Join Blueprint to gain hands on experience and give back to the community."}
-              redirect_url={"https://hobokenlibrary.org/"}
+              description={"Join Blueprint to gain hands on experience and give back to the community. Teeeee eeeee eeeeeee eeeeeessss ssssssss sssssss sssssss sssssss sssssttttt tttttt tttttt tting oooooo oooover flow. Here's some more stuff! Here's so much stuff! So much stuff, it'll overflow off the page!"}
+              redirect_url={"../blog/example1.html"}
               redirect_url_text={"Read More"}
         />
         <div className="grid grid-cols-3 gap-4">
