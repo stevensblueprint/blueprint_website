15075871fe71a3dd7e68c2b49e2fd12efd773c9e
Alexis Emerson <alexis.g.emerson@gmail.com>
2024-10-18 17:23:26 -0400

    All components but not completely formatted, truncating text issue

--- src/_components/BlogHero.jsx
+++ src/_components/BlogHero.jsx
@@ -10,29 +10,45 @@ export default ({
     redirect_url,
     redirect_url_text, }) => {
     return (
-      <figure className="flex-col flex w-full border border-black rounded-lg lg:p-5 p-8 lg:max-w-[1280px]">
-        <table className="table-fixed border">
-            <td className="w-8/12 border border-black">
-                <h1> {"Stevens Blueprint Blog"} </h1>
-                <p> {time_posted} </p>
-                <table> 
-                    <td className="border">{card_header}</td>
-                    <td className="border">{tags}</td>
-                </table>
-                <comp.Button
-                    style={"lg:mt-5 mt-4 flex justify-center items-center"} 
-                    href={redirect_url}
-                    text={redirect_url_text}
-                />
-                <p> {description} </p>
-                <div className="lg:mt-5 mt-4 flex justify-center items-center">
-                    <a
-                    className="text-primary font-semibold lg:text-2xl text-4xl"
-                    href={redirect_url}> {redirect_url_text} </a>
+      <figure className="flex-col flex w-full lg:p-5 p-8 lg:max-w-[1280px]">
+        <div className="flex">
+            <div className="w-2/3 pr-8">
+                {/* Left Side Content */}
+                <h1 className="pb-8 text-4xl text-primary font-normal"> {"Stevens Blueprint Blog"} </h1>
+                <div className="flex">
+                    <div>
+                    <img
+                        className="size-1"
+                        src="../assets/vector/clock.svg.svg"
+                        alt="Clock"
+                    />
+                    </div>
+                    <div className="text-xl font-normal"> <p> {time_posted} </p> </div>
                 </div>
-            </td>
-            <td className="border border-black">
-            {image_url && (
+                <div className="flex">
+                    <div className="text-3xl font-semibold pb-2 pt-2"> {card_header} </div>
+                    <div> {tags} </div>
+                <div className="flex flex-wrap">
+                            {tags.map((tag) => (
+                            <span
+                                key={tag}
+                                className="m-1 bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700"
+                            >
+                                {tag}
+                            </span>
+                        ))}
+                    </div>
+                </div>
+                <p className="flex-auto h-20 text-xl font-normal leading-7 text-balance truncate overflow-hidden text-clip"> {description} </p>
+                <comp.Button
+                    style={"block lg:px-8 px-8 lg:py-2 py-2 lg:rounded-md rounded-md border border-black text-base leading-normal font-bold w-fit lg:text-base text-4xl"} 
+                    text={"Read More"}
+                    redirect_url={redirect_url}
+                />
+            </div>
+            <div className="w-1/3 bg-transparent">
+                {/* Image */}
+                {image_url && (
                 <div className="flex justify-center items-center w-full">
                     <img
                     src={image_url}
@@ -41,8 +57,8 @@ export default ({
                     />
                     </div>
                 )}
-            </td>
-        </table>
+            </div>
+        </div>
       </figure>
     );
   }  
