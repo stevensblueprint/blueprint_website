cfe0d4124f6e889cc469dc55d79e38011d88e046
Maya <mpate154@stevens.edu>
2024-10-20 19:13:46 -0400

    changed github logo to square

--- src/_includes/_layouts/Home.jsx
+++ src/_includes/_layouts/Home.jsx
@@ -121,7 +121,7 @@ export default ({ comp, title }) => {
             redirect_url={"projects"}
           />
         </section>
-        <section className="h-[20rem] bg-white text-black flex flex-col justify-evenly items-left justify-center px-12 max-lg:w-full max-lg:flex-1 max-lg:px-8 py-20 max-lg:h-fit max-lg:gap-y-20 max-lg:py-16">
+        <section className="h-[24rem] bg-white text-black flex flex-col justify-evenly items-left justify-center px-12 max-lg:w-full max-lg:flex-1 max-lg:px-8 py-24 max-lg:h-fit max-lg:gap-y-20 max-lg:py-16">
           <h1 className="lg:text-5xl text-6xl mb-4">
             <strong>Our Sponsors</strong>
           </h1>
@@ -129,15 +129,15 @@ export default ({ comp, title }) => {
             We are grateful to our sponsors who provide us with the resources
             and support we need at Blueprint!
           </h2>
-          <div className="flex flex-wrap gap-x-6 justify-center px-8 max-lg:px-4 max-md:px-2 pt-2">
+          <div className="flex flex-wrap gap-x-16 justify-center px-8 max-lg:px-4 pt-9 max-md:px-2 pt-2">
             <img
-              className="object-contain h-20 max-w-none mr-4 mt-7"
+              className="object-contain h-20 max-w-none "
               src="../assets/logos/notion.png"
               alt="Notion logo"
             />
             <img
-              className="object-contain h-20 max-w-none mr-4 mt-7"
-              src="../assets/logos/github.png"
+              className="object-cover h-20 max-w-none "
+              src="../assets/logos/github_square.png"
               alt="GitHub logo"
             />
           </div>
