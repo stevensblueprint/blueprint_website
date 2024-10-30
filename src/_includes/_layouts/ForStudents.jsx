09d12234c245b7ed2b64cfae2ef23394991b6d8f
Christopher Engelbart <cengelbart39@protonmail.com>
2024-10-30 15:37:13 -0400

    Fix styling issues

--- src/_includes/_layouts/ForStudents.jsx
+++ src/_includes/_layouts/ForStudents.jsx
@@ -12,45 +12,45 @@ export default ({ comp }) => (
     </head>
     <body>
       <comp.Navbar />
-      <section className="grow flex flex-col lg:flex-row lg:items-center lg:min-h-[42rem] bg-secondary relative overflow-hidden">
+      <section className="grow flex flex-col lg:flex-row lg:items-center lg:min-h-[42rem] bg-primary relative overflow-hidden">
         <div className="lg:w-7/12 lg:pl-40 lg:pr-20 text-negative max-lg:w-full max-lg:px-8 max-lg:flex max-lg:flex-col max-lg:justify-center max-large:items-center">
-          <h1 className="lg:text-4xl text-7xl my-12 lg:px-0 px-10 tracking-wide max-lg:text-center text-[#00070E]">
+          <h1 className="lg:text-4xl text-7xl my-12 lg:px-0 px-10 tracking-wide max-lg:text-center">
             <strong>Students</strong>
           </h1>
-          <p className="lg:text-xl text-4xl mb-12 lg:px-0 px-10 tracking-wide max-lg:text-center text-[#00070E]">
+          <p className="lg:text-xl text-4xl mb-12 lg:px-0 px-10 tracking-wide max-lg:text-center">
             We’re proud of the diverse membership here at Blueprint. Join us and
             make an impact with our opportunities in product design, project
             management, or software development!
           </p>
           <div className="flex space-x-6 max-lg:flex-col max-lg:space-x-0 max-lg:space-y-8 max-lg:w-full max-lg:items-center">
             <comp.Button
-              style="px-8 py-4 rounded-lg bg-transparent border-2 border-[#00070E] text-[#00070E] font-semibold text-lg inline-block max-lg:px-12 max-lg:py-6 max-lg:text-4xl"
+              style="px-8 py-4 rounded-lg bg-transparent border-2 font-semibold text-lg inline-block max-lg:px-12 max-lg:py-6 max-lg:text-4xl"
               redirect_url={"https://discord.gg/G6aTAc9nFQ"}
               text={"Join Our Discord"}
             />
             <comp.Button
-              style="px-8 py-4 rounded-lg bg-transparent border-2 border-[#00070E] text-[#00070E] font-semibold text-lg inline-block max-lg:px-12 max-lg:py-6 max-lg:text-4xl"
+              style="px-8 py-4 rounded-lg bg-transparent border-2 font-semibold text-lg inline-block max-lg:px-12 max-lg:py-6 max-lg:text-4xl"
               redirect_url={"/projects"}
               text={"See Our Projects"}
             />
           </div>
         </div>
         <div className="lg:w-1/2 lg:pr-20 lg:pl-0 px-10 flex items-center justify-center py-8">
-          <div className="max-lg:p-8 lg:aspect-[4/3] lg:rounded-2xl lg:border-4 lg:border-solid lg:border-black lg:overflow-hidden">
+          <div className="max-lg:p-8 lg:rounded-2xl lg:overflow-hidden">
             <image
               src="../../assets/events/students_hero.jpg"
               alt="A photo of student Blueprint members"
-              className="w-full lg:h-full lg:object-cover max-lg:h-auto max-lg:object-contain max-lg:rounded-2xl max-lg:border-4 max-lg:border-solid max-lg:border-white"
+              className="w-full lg:h-full lg:object-cover max-lg:h-auto max-lg:object-contain max-lg:rounded-2xl"
             />
           </div>
         </div>
       </section>
       <section>
-        <h1 className="lg:text-5xl text-7xl flex flex-col items-center justify-center py-32 gap-y-6">
+        <h1 className="lg:text-5xl text-7xl flex flex-col items-center justify-center lg:pt-32 max-lg:py-32 gap-y-6">
           <strong>Our Teams</strong>
         </h1>
       </section>
-      <section className="grow flex flex-col lg:flex-row lg:items-center lg:min-h-[42rem] relative overflow-hidden">
+      <section className="grow flex flex-col lg:flex-row lg:items-center lg:min-h-[42rem] relative overflow-hidden lg:pb-32">
         <div className="lg:w-7/12 lg:pl-40 lg:pr-20 text-negative max-lg:w-full max-lg:px-8 max-lg:flex max-lg:flex-col max-lg:justify-start max-large:items-start">
           <h2 className="font-semibold lg:text-2xl text-5xl text-black">
             Project Teams
