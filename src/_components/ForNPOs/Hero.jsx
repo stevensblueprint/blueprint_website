2f81585ca4cf456e4d503ab3dc746fed6b821a2b
Caposto <christiand.apostol@outlook.com>
2024-11-02 23:30:34 -0400

    removed fixed sizing on text

--- src/_components/ForNPOs/Hero.jsx
+++ src/_components/ForNPOs/Hero.jsx
@@ -2,12 +2,12 @@ import React from "https://esm.sh/react";
 
 export default function Hero({ comp }) {
   return (
-    <section className="grow flex flex-col h-[400px] lg:flex-row lg:items-center relative overflow-hidden bg-primary text-white">
-      <div className="px-20">
-        <h1 className="font-bold text-5xl leading-[1.3] tracking-wide mb-8 max-lg:text-5xl max-lg:text-center max-lg:mb-8">
+    <section className="grow flex flex-col lg:flex-row lg:items-center relative overflow-hidden bg-primary text-white">
+      <div className="px-20 my-16">
+        <h1 className="font-bold text-5xl leading-[1.3] tracking-wide mb-8 max-lg:text-center max-lg:mb-8">
           Non-Profit Organizations
         </h1>
-        <p className="text-[1.5rem] w-[36rem] leading-[1.3] mb-16 font-weight:400">
+        <p className="text-2xl leading-[1.3] mb-14 font-weight:400 max-lg:text-center">
           We are passionate about leveraging our technical and
           <br />
           consulting skills to support your organization’s mission,
