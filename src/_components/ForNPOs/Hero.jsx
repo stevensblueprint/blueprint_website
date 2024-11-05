a026ac80964cb3c7ef95472ba96fe9153ffc66d8
Caposto <christiand.apostol@outlook.com>
2024-11-05 18:18:01 -0500

    expanded size of hero

--- src/_components/ForNPOs/Hero.jsx
+++ src/_components/ForNPOs/Hero.jsx
@@ -2,9 +2,9 @@ import React from "https://esm.sh/react";
 
 export default function Hero({ comp }) {
   return (
-    <section className="grow flex flex-col min-h-[400px] lg:flex-row lg:items-center relative overflow-hidden bg-primary text-white">
-      <div className="px-6 md:px-12 lg:px-20 py-8 md:py-16 lg:py-0">
-        <h1 className="font-bold text-3xl md:text-4xl lg:text-5xl leading-snug md:leading-[1.3] tracking-wide mb-4 md:mb-6 lg:mb-8 text-center lg:text-left">
+    <section className="grow flex flex-col min-h-[700px] lg:flex-row lg:items-center relative overflow-hidden bg-primary text-white">
+      <div className="px-6 md:px-12 lg:px-32 py-8 md:py-16 lg:py-0">
+        <h1 className="font-bold text-3xl md:text-4xl lg:text-5xl leading-snug md:leading-[1.3] tracking-wide mb-4 md:mb-6 lg:mb-16 text-center lg:text-left">
           Non-Profit Organizations
         </h1>
         <p className="text-lg md:text-xl lg:text-2xl leading-snug md:leading-[1.3] mb-6 md:mb-10 lg:mb-14 text-center lg:text-left">
@@ -16,14 +16,14 @@ export default function Hero({ comp }) {
         </p>
         <div className="flex flex-col md:flex-row md:justify-center lg:justify-start space-y-4 md:space-y-0 md:space-x-6">
           <comp.Button
-            style="h-10 px-6 md:px-8 py-4 rounded-md border border-white justify-center items-center gap-2 text-white font-semibold text-base md:text-lg inline-flex"
-            text="See our Projects"
-            redirect_url="../../projects" // FIXME: How can we avoid hardcoding the URL?
-          />
-          <comp.Button
-            style="h-10 px-6 md:px-8 py-4 rounded-md border border-white justify-center items-center gap-2 text-white font-semibold text-base md:text-lg inline-flex"
+            style="h-10 px-6 md:px-8 min-w-[191px] rounded-md border border-white justify-center items-center gap-2 text-white font-semibold text-base md:text-lg inline-flex"
             text="Email Us"
             redirect_url="community"
+          />
+          <comp.Button
+            style="h-10 px-6 md:px-8 min-w-[191px] rounded-md border border-white justify-center items-center gap-2 text-white font-semibold text-base md:text-lg inline-flex"
+            text="See our Projects"
+            redirect_url="../../projects" // FIXME: How can we avoid hardcoding the URL?
           />
         </div>
       </div>
