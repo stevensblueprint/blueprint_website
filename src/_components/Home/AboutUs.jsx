ad2be34cff13ede574fe6cd1bcdee8e453c4f63a
Christian Apostol <christiand.apostol@outlook.com>
2025-01-20 17:50:48 -0500

    done

--- src/_components/Home/AboutUs.jsx
+++ src/_components/Home/AboutUs.jsx
@@ -1,6 +1,6 @@
 import React from "https://esm.sh/react";
 
-export default function AboutUs({ comp, about_title, about_text }) {
+export default function AboutUs({ comp, about }) {
   return (
     <section className="relative">
       <div>
@@ -17,10 +17,10 @@ export default function AboutUs({ comp, about_title, about_text }) {
     bg-primary text-white text-center p-4"
         >
           <h2 className="hidden font-bold lg:block text-2xl md:text-4xl my-6 md:mb-12">
-            {about_title}
+            {about.title}
           </h2>
           <p className="hidden lg:block text-base md:text-lg mb-6 md:mb-12">
-            {about_text}
+            {about.description}
           </p>
           <comp.Button
             style="font-bold px-2 py-1 md:text-xl"
--- src/_includes/_layouts/Home.jsx
+++ src/_includes/_layouts/Home.jsx
@@ -16,10 +16,7 @@ export default ({ comp, title, about }) => {
       <body>
         <comp.Navbar />
         <comp.Home.Hero />
-        <comp.Home.AboutUs
-          about_title={about.title}
-          about_text={about.description}
-        />
+        <comp.Home.AboutUs about={about} />
         <comp.Home.Impact />
         <section className="flex flex-col items-center justify-center py-32 gap-y-6 bg-white">
           <h1 className="lg:text-5xl text-7xl">
