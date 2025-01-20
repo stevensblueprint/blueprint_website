b10ff98c1c8a565e59a7b888539657d8ee5becea
miguel-merlin <mmerlin@stevens.edu>
2025-01-20 18:26:21 -0500

    feat: added project gallery

--- src/_includes/_layouts/Projects.jsx
+++ src/_includes/_layouts/Projects.jsx
@@ -1,4 +1,4 @@
-export default ({ comp }) => (
+export default ({ comp, projects }) => (
   <html>
     <head>
       <comp.OpenGraphCommon />
@@ -11,7 +11,32 @@ export default ({ comp }) => (
       <div>
         <comp.Navbar />
         <comp.Projects.Hero />
-        <comp.Projects.Projects />
+        <section className="flex flex-col items-center justify-center pt-20 pb-10 gap-y-6 bg-white">
+          <h1 className="lg:text-5xl text-7xl">
+            <strong>Project Gallery</strong>
+          </h1>
+          <p className="text-center text-2xl lg:text-2xl text-2xl mb-4 lg:px-14 px-24">
+            Interested in collaborating with other peers at Stevens on a
+            project? <br /> Check out our <a href="/students" className="underline">student opportunities</a>
+            !
+          </p>
+        </section>
+        <div className="container mx-auto px-4 py-10">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-10 place-items-center">
+            {projects.map((project, idx) => (
+              <div key={idx} className="w-full flex justify-center">
+                <comp.ProjectCard
+                  name_organization={project.name_organization}
+                  image_url={project.image_url}
+                  status={project.status}
+                  description={project.description}
+                  redirect_url={project.redirect_url}
+                  project_tag={project.project_tag}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
         <comp.Footer />
       </div>
     </body>
