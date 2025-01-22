6b5009fdb65b1e2a63855490004692c241573e0f
Christian Apostol <christiand.apostol@outlook.com>
2025-01-21 20:00:00 -0500

    adjusted padding to get npo header text on one line

--- src/_components/ForNPOs/Hero.jsx
+++ src/_components/ForNPOs/Hero.jsx
@@ -3,7 +3,7 @@ import React from "https://esm.sh/react@19.0.0";
 export default function Hero({ comp }) {
   return (
     <section className="grow flex space-between flex-col min-h-[700px] lg:flex-row lg:items-center relative overflow-hidden bg-primary text-white px-12 lg:px-40 ">
-      <div className="py-8 md:py-16 lg:py-0 w-full lg:pr-20">
+      <div className="py-8 md:py-16 lg:py-0 w-full lg:pr-16">
         <h1 className="font-bold text-3xl md:text-4xl lg:text-5xl leading-snug md:leading-[1.3] tracking-wide mb-4 md:mb-6 lg:mb-16 text-center lg:text-left">
           Non-Profit Organizations
         </h1>
@@ -18,13 +18,13 @@ export default function Hero({ comp }) {
           />
           <comp.HeroButton
             text="See our Projects"
-            redirect_url="../../projects" // FIXME: How can we avoid hardcoding the URL?
+            redirect_url="/projects"
           />
         </div>
       </div>
       <div className="flex grow w-full justify-center pb-8">
         <img
-          src="../../assets/vector/hands.svg" // FIXME: Avoid absolute path?
+          src="../../assets/vector/hands.svg"
           alt="hands"
         />
       </div>
