014f055daaef4778c0b68d9f029b78efbd3ca023
Justin Ferber <119906373+j-ferber@users.noreply.github.com>
2025-01-22 14:56:52 -0500

    Fix/react md (#276)
    * Fix for react-markdown package
    
    * formatting fix
    
    * Added deno.lock file
    
    * formatting
    
    * Fixed incorrect import statement

--- src/_components/DropdownFAQ.jsx
+++ src/_components/DropdownFAQ.jsx
@@ -1,11 +1,7 @@
-import React from "https://esm.sh/react";
-import MarkdownIt from "https://esm.sh/markdown-it";
-
-const md = new MarkdownIt();
+import React from "react";
+import ReactMarkdown from "react-markdown";
 
 export default function DropdownFAQ({ faq, index }) {
-  const answerHtmlContent = md.render(faq.answer);
-
   return (
     <div className="flex flex-col justify-center items-center w-full first-of-type:border-t-[1px] border-b-[1px] border-black">
       <input
@@ -24,12 +20,10 @@ export default function DropdownFAQ({ faq, index }) {
           className="h-[7px] w-3 peer-checked:group-[]:rotate-180"
         />
       </label>
-      {/* Smooth Animation Requires number for max-height, choose arbitrarily large number */}
-      <div className="overflow-hidden max-h-0 peer-checked:max-h-96 w-full transition-all duration-300 px-3 peer-checked:pt-2 peer-checked:pb-[20px]">
-        <div
-          className="prose prose-p:text-xl prose-p:leading-6 max-w-none"
-          dangerouslySetInnerHTML={{ __html: answerHtmlContent }}
-        />
+      <div className="overflow-hidden max-h-0 peer-checked:max-h-max w-full transition-all duration-300 px-3 peer-checked:pt-2 peer-checked:pb-[20px]">
+        <div className="prose prose-p:text-xl prose-p:leading-6 max-w-none">
+          <ReactMarkdown>{faq.answer}</ReactMarkdown>
+        </div>
       </div>
     </div>
   );
