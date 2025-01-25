7130fcd6a1fae501865a9dbd7703aca5aea46c66
Alexis Emerson <alexis.g.emerson@gmail.com>
2025-01-25 16:28:46 -0500

    fixed uneven values headers

--- src/_includes/_layouts/About.jsx
+++ src/_includes/_layouts/About.jsx
@@ -36,10 +36,10 @@ export default ({ comp, values, team }) => (
         />
         <section className="flex place-content-center" id="info">
           <div className="flex flex-col items-center py-20 max-lg:px-12max-md:px-4">
-            <h1 className="leading-10 md:text-[40px] mb-14 text-6xl">
+            <h1 className="leading-10 justify-center md:text-[40px] mb-14 text-6xl">
               <strong>Our Values</strong>
             </h1>
-            <div className="flex flex-row items-center p-20 gap-10">
+            <div className="flex flex-col sm:flex-row p-20 gap-10">
               {values.map((value, index) => (
                 <comp.ValueCard key={index} value={value} />
               ))}
