d47fd7bafd75ae12f05d379679bc85cb04d4b5bc
Alexis Emerson <alexis.g.emerson@gmail.com>
2025-01-29 16:57:22 -0500

    added md:w-1/3 and modified text sizes

--- src/_components/GetInvolvedCard.jsx
+++ src/_components/GetInvolvedCard.jsx
@@ -7,7 +7,7 @@ export default function GetInvolvedCard({
   redirect_url,
 }) {
   return (
-    <figure className="flex flex-col border border-black rounded-lg lg:p-5 p-5 lg:h-[450px]">
+    <figure className="flex flex-col md:w-1/3 border border-black rounded-lg lg:p-5 p-5 lg:h-[450px]">
       <div className="h-36 flex justify-center items-center w-full shrink-0">
         <img
           src={icon_url}
@@ -17,11 +17,11 @@ export default function GetInvolvedCard({
       </div>
       <figcaption className="flex flex-col flex-1">
         <div className="flex-1">
-          <h1 className="font-semibold lg:text-2xl text-5xl mt-4 mb-3">
+          <h1 className="font-semibold lg:text-2xl text-3xl mt-4 mb-3">
             {title}
           </h1>
           <p
-            className="line-clamp-6 max-sm:w-full lg:text-xl text-3xl lg:leading-6 overflow-hidden"
+            className="line-clamp-6 max-sm:w-full lg:text-xl text-xl lg:leading-6 overflow-hidden"
             style={{ maxHeight: "6em" }}
           >
             {description}
