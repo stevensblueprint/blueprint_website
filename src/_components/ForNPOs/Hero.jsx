cb4de275f4d32dc255226cbfec13a837bca1fee4
Shawn Aviles <shawnaviles26@gmail.com>
2025-02-03 13:40:51 -0500

    fix: left align hero

--- src/_components/ForNPOs/Hero.jsx
+++ src/_components/ForNPOs/Hero.jsx
@@ -4,14 +4,14 @@ export default function Hero({ comp }) {
   return (
     <section className="grow flex space-between flex-col min-h-[700px] lg:flex-row lg:items-center relative overflow-hidden bg-primary text-white px-6 md:px-12 lg:px-32">
       <div className="py-8 md:py-16 lg:py-0 w-full lg:pr-16">
-        <h1 className="font-bold text-3xl md:text-4xl lg:text-5xl leading-snug md:leading-[1.3] tracking-wide mb-4 md:mb-6 lg:mb-16 text-center lg:text-left">
+        <h1 className="font-bold text-3xl md:text-4xl lg:text-5xl leading-snug md:leading-[1.3] tracking-wide mb-4 md:mb-6 lg:mb-16">
           Non-Profit Organizations
         </h1>
-        <p className="text-lg md:text-xl lg:text-2xl leading-snug md:leading-[1.3] mb-6 md:mb-10 lg:mb-14 text-center lg:text-left">
+        <p className="text-lg md:text-xl lg:text-2xl leading-snug md:leading-[1.3] mb-6 md:mb-10 lg:mb-14">
           We are passionate about leveraging our technical and consulting skills
           to support your organization’s mission, free of charge!
         </p>
-        <div className="flex flex-col md:flex-row md:justify-center lg:justify-start space-y-4 md:space-y-0 md:space-x-6">
+        <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-6">
           <comp.HeroButton
             text="Email Us"
             redirect_url="mailto:sit.blueprint@gmail.com"
