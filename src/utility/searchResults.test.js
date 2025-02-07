7508b23acad8d29f87281922dadc5665adeb20b8
Christian Apostol <christiand.apostol@outlook.com>
2025-02-06 21:45:37 -0500

    added new test

--- src/utility/searchResults.test.js
+++ src/utility/searchResults.test.js
@@ -2,10 +2,21 @@ import { assertEquals } from "jsr:@std/assert";
 
 function getBlogsByTitle(searchTerm, blogPosts) {
   const results = blogPosts.filter(
-    (post) => post.title.includes(searchTerm), // Fix: to lower
+    (post) => post.title.toLowerCase().includes(searchTerm.toLowerCase()), // Fix: to lower
   );
   return results;
 }
+
+Deno.test("Searching an array with multiple blogs", () => {
+  const blogs = [
+    { title: "test blog" },
+    { title: "another Test blog" },
+    { title: "Yet Another Test blog" },
+  ];
+  const result = getBlogsByTitle("test", blogs);
+  assertEquals(result, blogs);
+});
+
 
 Deno.test("Searching an empty array", () => {
   const blogs = [];
@@ -18,5 +29,3 @@ Deno.test("Searching an array with one blog", () => {
   const result = getBlogsByTitle("test", blogs);
   assertEquals(result, blogs);
 });
-
-// Add TEST
