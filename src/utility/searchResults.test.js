95ac2549bc34c38bc24704feb9230be5a669b9f8
Christian Apostol <christiand.apostol@outlook.com>
2025-02-06 19:54:57 -0500

    update test workflow:

--- src/utility/searchResults.test.js
+++ src/utility/searchResults.test.js
@@ -1,8 +1,8 @@
 import { assertEquals } from "jsr:@std/assert";
 
 function getBlogsByTitle(searchTerm, blogPosts) {
-  const results = blogPosts.filter((post) =>
-    post.title.includes(searchTerm) // Fix: to lower
+  const results = blogPosts.filter(
+    (post) => post.title.includes(searchTerm), // Fix: to lower
   );
   return results;
 }
