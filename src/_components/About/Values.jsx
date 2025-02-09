23f9d37fe50dc32f927b1aa21bd85d80b8dcf38e
Shawn Aviles <shawnaviles26@gmail.com>
2025-02-09 02:12:27 -0500

    style: about page section responsiveness and refactor

---
+++ src/_components/About/Values.jsx
@@ -0,0 +1,16 @@
+export default function Values({ comp, values }) {
+  return (
+    <section className="flex place-content-center" id="info">
+      <div className="flex flex-col items-center py-20 px-8 lg:px-40">
+        <h1 className="mb-12 text-4xl lg:text-5xl">
+          <strong>Our Values</strong>
+        </h1>
+        <div className="flex flex-col sm:flex-row lg:p-10 gap-14">
+          {values.map((value, index) => (
+            <comp.ValueCard key={index} value={value} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/_components/MemberCard.jsx
+++ src/_components/MemberCard.jsx
@@ -2,19 +2,19 @@ import React from "react";
 
 export default function MemberCard({ name, role, image_url, linkedin }) {
   return (
-    <figure className="flex flex-col items-center p-4">
+    <figure className="flex flex-col items-center lg:p-4">
       <img
-        className="rounded-lg mb-4 object-cover object-center lg:w-32 lg:h-32 w-80 h-80"
+        className="rounded-lg mb-4 object-cover object-center lg:w-32 lg:h-32 w-40 h-40"
         src={image_url}
         alt="member image"
       />
       <figcaption className="text-center">
-        <div className="font-semibold lg:text-base text-3xl">{name}</div>
-        <div className="lg:text-base text-3xl">{role}</div>
+        <div className="font-semibold lg:text-base text-xl">{name}</div>
+        <div className="lg:text-base text-xl">{role}</div>
       </figcaption>
       <a href={linkedin}>
         <img
-          className="mb-4 object-cover object-center lg:w-8 lg:h-8 w-12 h-12"
+          className="mb-4 mt-1 object-cover object-center w-8 h-8"
           src="../assets/logos/linkedin.png"
           alt="LinkedIn Logo"
         />
