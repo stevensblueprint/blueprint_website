a44c20fc84af36aec9f5beedc2868c60f8ee966e
Shawn Aviles <shawnaviles26@gmail.com>
2025-02-09 02:09:50 -0500

    style: consistent hero styling for mobile

--- src/_components/About/Hero.jsx
+++ src/_components/About/Hero.jsx
@@ -2,12 +2,12 @@ import React from "https://esm.sh/react";
 
 export default function Hero({ comp }) {
   return (
-    <section className="grow flex flex-col min-h-[700px] lg:flex-row lg:items-center overflow-hidden bg-primary text-white">
-      <div className="px-6 md:px-12 lg:pl-32 py-8 md:py-16 lg:py-0 w-full">
-        <h1 className="font-bold text-4xl lg:text-5xl leading-snug md:leading-[1.3] tracking-wide mb-4 md:mb-6 lg:mb-16 text-center lg:text-left">
+    <section className="grow flex flex-col min-h-[700px] lg:flex-row lg:items-center overflow-hidden bg-primary text-white px-6 md:px-12 lg:px-32">
+      <div className="py-8 md:py-16 lg:py-0 w-full lg:w-1/2 lg:pr-20">
+        <h1 className="font-bold text-4xl lg:text-5xl leading-snug md:leading-[1.3] tracking-wide mb-4 md:mb-6 lg:mb-16">
           About Us
         </h1>
-        <p className="text-[25px] md:text-xl lg:text-2xl leading-snug md:leading-[1.3] mb-6 md:mb-10 lg:mb-14 text-left">
+        <p className="text-lg md:text-xl lg:text-2xl leading-snug md:leading-[1.3] mb-6 md:mb-10 lg:mb-14 text-left">
           We are the Stevens Institute of Technology chapter of Blueprint that
           develops pro-bono software for non-profit organizations and promotes
           tech for social good.
@@ -21,11 +21,11 @@ export default function Hero({ comp }) {
           <comp.HeroButton text="See our projects" redirect_url="/projects" />
         </div>
       </div>
-      <div className="flex px-6 md:px-12 lg:32 grow w-full justify-center pb-12">
+      <div className="flex grow w-full lg:w-1/2 justify-center pb-8 md:pb-12">
         <image
           src="/assets/events/google_event_1_cropped.jpg"
           alt="A photo of Blueprint members at a Google Event in New York City"
-          className="rounded-[20px]"
+          className="rounded-2xl w-full h-auto object-cover"
         />
       </div>
     </section>
