5e02a3f16387bbc79ab81c7cea736386efcfdf17
Shawn Aviles <shawnaviles26@gmail.com>
2025-02-11 02:11:45 -0500

    style: consistent padding, small refactor, default 2xl shadow on mobile

--- src/_includes/_layouts/Projects.jsx
+++ src/_includes/_layouts/Projects.jsx
@@ -6,16 +6,20 @@ export default ({ comp, projects }) => (
       <meta property="og:url" content="https://sitblueprint.com/projects/" />
       <link rel="stylesheet" href="/styles.css" />
       <title>Stevens Blueprint</title>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0"
+      ></meta>
     </head>
     <body>
       <div>
         <comp.Navbar />
         <comp.Projects.Hero />
-        <section className="flex flex-col items-center justify-center pt-20 pb-10 gap-y-6 bg-white">
-          <h1 className="lg:text-5xl text-7xl">
+        <section className="flex flex-col lg:items-center justify-center pt-20 lg:pb-10 gap-y-2 lg:gap-y-6 bg-white px-8">
+          <h1 className="text-4xl lg:text-5xl">
             <strong>Project Gallery</strong>
           </h1>
-          <p className="text-center text-2xl lg:text-2xl text-2xl mb-4 lg:px-14 px-24">
+          <p className="lg:text-center text-xl lg:text-3xl lg:mb-4 max-w-4xl py-8">
             Interested in collaborating with other peers at Stevens on a
             project? <br /> Check out our{" "}
             <a href="/students" className="underline">
@@ -24,11 +28,11 @@ export default ({ comp, projects }) => (
             !
           </p>
         </section>
-        <div className="container mx-auto px-4 py-10">
-          <section className="flex justify-center mb-20">
+        <div className="container mx-auto px-8 py-10 mb-16">
+          <section className="flex justify-center mb-8 lg:mb-20">
             <comp.FilterButtons />
           </section>
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 max-w-7xl mx-auto px-4">
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 max-w-7xl mx-auto">
             {projects.map((project, idx) => (
               <div key={idx} className="w-full flex justify-center">
                 <comp.ProjectCard
