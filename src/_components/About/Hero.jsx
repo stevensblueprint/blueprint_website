ca1b28dec9d93faaa9c6a14c37886bad0d77e044
Christian Apostol <christiand.apostol@outlook.com>
2025-02-19 00:24:49 -0500

    adjusted mobile padding for about hero image

--- src/_components/About/Hero.jsx
+++ src/_components/About/Hero.jsx
@@ -21,7 +21,7 @@ export default function Hero({ comp }) {
           <comp.HeroButton text="See our projects" redirect_url="/projects" />
         </div>
       </div>
-      <div className="flex w-full lg:w-1/2 justify-center p-4 sm:p-16">
+      <div className="flex w-full lg:w-1/2 justify-center p-2 sm:p-16">
         <img
           src="/assets/photos/stock_photo.png"
           alt="A photo of Blueprint members hard at work"
