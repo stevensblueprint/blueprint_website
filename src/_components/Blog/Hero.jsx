ad8c229f8ec6b3cb201ce871412b3e2609129f18
Christian Apostol <christiand.apostol@outlook.com>
2025-02-19 14:25:39 -0500

    Implemented Mobile responsive blog hero

--- src/_components/Blog/Hero.jsx
+++ src/_components/Blog/Hero.jsx
@@ -10,66 +10,53 @@ export default function Hero({
   redirect_url,
 }) {
   return (
-    <figure className="flex-col flex w-full lg:p-5 p-8 lg:max-w-[1280px]">
-      <div className="flex">
-        <div className="w-2/3 pr-8">
-          {/* Left Side Content */}
-          <h1 className="pb-8 text-4xl text-primary font-normal">
-            {" "}
-            {"Stevens Blueprint Blog"}{" "}
-          </h1>
-          <div className="flex content-center">
-            <div>
-              <img
-                className="size-1"
-                src="../assets/vector/clock.svg.svg"
-                alt="Clock"
-              />
-            </div>
-            <div className="text-base font-normal">
-              <p> {time_posted} </p>
-            </div>
-          </div>
-          <div className="flex">
-            <div className="text-3xl font-semibold pb-4 pt-2">
-              {card_header}
-            </div>
-            <div className="flex flex-wrap grid-cols-2 gap-1 justify-center content-center">
-              {tags.map((tag) => (
-                <div
-                  key={tag}
-                  className="bg-primary rounded-md px-3 text-sm font-semibold text-white text-center truncate"
-                >
-                  {tag}
-                </div>
-              ))}
-            </div>
-          </div>
-          <p className="flex-auto text-xl font-normal leading-7 text-balance overflow-hidden text-clip line-clamp-3">
-            {description}
-          </p>
-          <div className="py-6">
-            <comp.Button
-              style={
-                "block lg:px-8 px-8 lg:py-2 py-2 lg:rounded-md rounded-md border border-black text-base leading-normal font-bold w-fit lg:text-base text-4xl"
-              }
-              text={"Read More"}
-              redirect_url={redirect_url}
-            />
+    <section className="grow flex flex-col min-h-[700px] lg:flex-row lg:items-center overflow-hidden px-6 md:px-12 lg:px-32">
+      <div className="py-8 md:py-16 lg:py-0 w-full lg:w-1/2 lg:pr-20">
+        <h1 className="font-bold text-primary text-3xl lg:text-5xl leading-snug md:leading-[1.3] tracking-wide mb-4 md:mb-6 lg:mb-16">
+          Stevens Blueprint Blog
+        </h1>
+        <div className="flex items-center">
+          <img
+            className="size-3"
+            src="../assets/vector/clock.svg"
+            alt="Clock"
+          />
+          <p className="text-base font-normal px-2">{time_posted}</p>
+          <div className="flex gap-1 content-center">
+            {tags.map((tag) => (
+              <div
+                key={tag}
+                className="bg-primary rounded-md px-3 text-sm font-semibold text-white text-center truncate"
+              >
+                {tag}
+              </div>
+            ))}
           </div>
         </div>
-        <div className="w-1/3 bg-transparent content-center">
-          {image_url && (
-            <div className="flex justify-center items-center w-full">
-              <img
-                src={image_url}
-                alt={`${card_header} Image`}
-                className="h-full w-full object-cover rounded-xl lg:max-w-[429px]"
-              />
-            </div>
-          )}
+        <div className="text-xl sm:text-3xl font-semibold pb-4 pt-2">
+          {card_header}
+        </div>
+        <p className="flex-auto text-lg sm:text-xl font-normal leading-7 text-balance overflow-hidden text-clip line-clamp-3">
+          {description}
+        </p>
+        {/* This div may seem pointless, however it is needed to apply padding between the 
+        button and the paragraph. If you add bottom padding to the paragraph, it causes a 
+        visual gltich with the clamped lines */}
+        <div className="pt-4">
+          <comp.Button
+            style="text-xs sm:min-w-[191px] md:text-base text-center py-1 md:py-2 px-3 md:px-4 rounded-md border bg-primary text-white font-semibold inline-flex items-center justify-center hover:bg-white hover:text-primary"
+            text={"Read More"}
+            redirect_url={redirect_url}
+          />
         </div>
       </div>
-    </figure>
+      <div className="flex grow w-full lg:w-1/2 justify-center pb-8 md:pb-12">
+        <image
+          src={image_url}
+          alt={`${card_header} Image`}
+          className="rounded-2xl w-full h-auto object-cover"
+        />
+      </div>
+    </section>
   );
-};
+}
--- src/_includes/_layouts/Blog.jsx
+++ src/_includes/_layouts/Blog.jsx
@@ -5,21 +5,29 @@ export default ({ comp, description, posts, children }) => (
       <meta property="og:title" content="Stevens Blueprint Blog" />
       <meta property="og:url" content="https://sitblueprint.com/blog/" />
       <link rel="stylesheet" href="/styles.css" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0"
+      ></meta>
       <title>Stevens Blueprint Blog</title>
     </head>
     <body>
       <comp.Navbar />
+      {/* The Blog Hero contains content for a "featured" post. 
+      This post should be updated periodically
+      to highlight certain topics or events. 
+      TODO: Is there a better way to pass content to the featured blog post?*/}
+      <comp.Blog.Hero
+        image_url={"../assets/events/spring_kickoff.png"}
+        card_header={"Reflecting on the 2023-2024 Year"}
+        tags={["Yearly Recap", "Blueprint"]} // Render at most 2 tags (21 total characters or less including spaces)
+        time_posted={"24 May 2024"}
+        description={
+          "As the spring semester comes to an end, we reflect on our work for the 2023-2024 school year. As the spring semester comes to an end, we reflect on our work for the 2023-2024 school year. As the spring semester comes to an end, we reflect on our work for the 2023-2024 school year. As the spring semester comes to an end, we reflect on our work for the 2023-2024 school year."
+        }
+        redirect_url={"../blog/yearEnd2024.html"}
+      />
       <main className="max-w-4xl mx-auto p-4">
-        <comp.Blog.Hero
-          image_url={"../assets/events/spring_kickoff.png"}
-          card_header={"Reflecting on the 2023-2024 Year"}
-          tags={["Yearly Recap"]}
-          time_posted={"24 May 2024"}
-          description={
-            "As the spring semester comes to an end, we reflect on our work for the 2023-2024 school year. As the spring semester comes to an end, we reflect on our work for the 2023-2024 school year."
-          }
-          redirect_url={"../blog/yearEnd2024.html"}
-        />
         <div className="grid grid-cols-3 gap-4">
           {/* 'posts' is currently a static string array that matches the filenames in blogs/ folder. Needs to be dynamic. See utility/searchResults.js for more info */}
           {posts.map((post) => (
@@ -32,6 +40,7 @@ export default ({ comp, description, posts, children }) => (
           ))}
         </div>
         <div className="flex justify-center items-center">
+          {/* FIXME: Might cut this for the initial launch pagination and filtering might not be the best here */}
           <button
             type="button"
             className="text-black font-bold hover:text-black border-2 border-black hover:bg-black rounded-lg text-sm px-10 py-2.5 text-center me-2 mb-2 dark:border-black dark:text-black dark:hover:text-white dark:hover:bg-black mb-20"
