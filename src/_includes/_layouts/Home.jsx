9c1c425b0d9e487b6359f199979712b51f64c1b0
Shawn Aviles <shawnaviles26@gmail.com>
2025-02-19 17:07:37 -0500

    refactor: home componentize refactor

--- src/_includes/_layouts/Home.jsx
+++ src/_includes/_layouts/Home.jsx
@@ -18,82 +18,8 @@ export default ({ comp, title, about }) => {
         <comp.Home.Hero />
         <comp.Home.AboutUs about={about} />
         <comp.Home.Impact />
-        <section className="flex flex-col items-center justify-center px-10 py-32 gap-y-6 bg-white">
-          <h1 className="lg:text-5xl text-7xl text-center">
-            <strong>Our Projects</strong>
-          </h1>
-          <p className="text-center text-2xl lg:text-2xl text-2xl mb-4 lg:px-14 px-24">
-            We collaborate with non-profit organizations and offer <br /> our
-            expertise in software development , web design, <br /> and product
-            management to bring ideas to life
-          </p>
-          <p className="text-center-2xl lg:text-2xl text-2xl text-primary hover:underline">
-            <a href="projects">View all projects</a>
-          </p>
-          <div className="flex flex-col md:flex-row md:max-w-[1440px] md:mx-auto gap-6 md:gap-x-6 lg:gap-x-12 flex-nowrap lg:gap-y-6">
-            <comp.ProjectCard
-              name_organization={"Sarapis"}
-              image_url={"../assets/logos/sarapis_logo.png"}
-              description={
-                "Sarapis is nonprofit organization advancing the free/libre/open source movement by helping NGOs, community groups and governments solve problems managing human services information."
-              }
-              redirect_url={"/projects/sarapis"}
-              project_tag={"Data Management"}
-              status={"In Progress"}
-            />
-            <comp.ProjectCard
-              name_organization={"Asian American Dream"}
-              image_url={"../assets/logos/aad_logo.png"}
-              description={
-                "Asian American Dream empowers underserved Asian American and Pacific Islander undergraduates through career-centric mentorship and professional development opportunities."
-              }
-              redirect_url={"/projects/asian-american-dream"}
-              project_tag={"Volunteer Management"}
-              status={"Completed"}
-            />
-            <comp.ProjectCard
-              name_organization={"InReach"}
-              image_url={"../assets/logos/inreach_logo.png"}
-              description={
-                "InReach provides verified services for LGBTQ+ asylum seekers, immigrants, refugees, BIPOC communities, TGNC individuals, LGBTQ+ youth, caregivers, and more."
-              }
-              redirect_url={"/projects/inreach"}
-              project_tag={"Community Resources"}
-              status={"Completed"}
-            />
-          </div>
-        </section>
-        <section className="flex flex-col items-center justify-center px-10 py-10 gap-y-6 bg-white">
-          <h1 className="lg:text-5xl text-7xl text-center">
-            <strong>Get Involved</strong>
-          </h1>
-          <div className="flex flex-col md:flex-row md:max-w-[1440px] md:mx-auto justify-between gap-6 md:gap-x-6 lg:gap-x-12 flex-nowrap lg:gap-y-6">
-            <comp.GetInvolvedCard
-              title={"Non-Profits"}
-              description={
-                "We can help you grow your organization with innovative software solutions, whether you have a specific project in mind or a curiosity of how Blueprint can support you!"
-              }
-              icon_url={"../assets/vector/hands_blue.svg"}
-              redirect_url={"/non-profits"}
-            />
-            <comp.GetInvolvedCard
-              title={"Students"}
-              description={
-                "We invite you to join our Blueprint family as a student volunteer and contribute to the design, development, or management of our projects!"
-              }
-              icon_url={"../assets/vector/student.svg"}
-              redirect_url={"/students"}
-            />
-            <comp.GetInvolvedCard
-              title={"Partnerships"}
-              description={
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet."
-              }
-              icon_url={"../assets/vector/partnership_handshake.svg"}
-              redirect_url={"/partners"}
-            />
-          </div>
-        </section>
+        <comp.Home.OurProjects />
+        <comp.Home.GetInvolved />
         <comp.Home.ContactUs />
         <comp.Footer />
       </body>
