8ab988310889d8d937f7ddf320a4b9c30dbb65f2
Christopher Engelbart <67113079+cengelbart39@users.noreply.github.com>
2025-02-22 16:02:22 -0500

    Add Hero Component (#322)
    The new component is implemented across the About, Projects, Students, and Non-Profits pages.

--- src/_components/Hero.jsx
+++ src/_components/Hero.jsx
@@ -1,26 +1,33 @@
 import React from "react";
 
-export default function Hero({ comp }) {
+export default function Hero({
+  comp,
+  title,
+  description,
+  button1,
+  button2,
+  image,
+}) {
   return (
     <section className="grow flex space-between flex-col min-h-[700px] lg:flex-row lg:items-center relative overflow-hidden bg-primary text-white px-6 md:px-12 lg:px-32">
       <div className="py-8 md:py-16 lg:py-0 w-full lg:pr-16">
         <h1 className="font-bold text-3xl md:text-4xl lg:text-5xl leading-snug md:leading-[1.3] tracking-wide mb-4 md:mb-6 lg:mb-16">
-          Non-Profit Organizations
+          {title}
         </h1>
         <p className="text-lg md:text-xl lg:text-2xl leading-snug md:leading-[1.3] mb-6 md:mb-10 lg:mb-14">
-          We are passionate about leveraging our technical and consulting skills
-          to support your organization’s mission, free of charge!
+          {description}
         </p>
         <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-6">
-          <comp.HeroButton
-            text="Email Us"
-            redirect_url="mailto:sit.blueprint@gmail.com"
-          />
-          <comp.HeroButton text="See our Projects" redirect_url="/projects" />
+          <comp.HeroButton text={button1.text} redirect_url={button1.url} />
+          <comp.HeroButton text={button2.text} redirect_url={button2.url} />
         </div>
       </div>
-      <div className="flex grow w-full justify-center pb-8">
-        <img src="../../assets/vector/hands.svg" alt="hands" />
+      <div className="flex sm:w-full lg:w-3/4 justify-center p-2 sm:p-16 lg:max-w-[600px] lg:max-h-[600px]">
+        <img
+          src={image.src}
+          alt={image.alt}
+          className={image.className ?? ""}
+        />
       </div>
     </section>
   );
--- src/_includes/_layouts/ForNPOs.jsx
+++ src/_includes/_layouts/ForNPOs.jsx
@@ -15,25 +15,36 @@ export default ({
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     </head>
     <body>
-      <>
-        <comp.Navbar />
-        <comp.ForNPOs.Hero />
-        <comp.ForNPOs.ProjectTimeline projectTimeline={projectTimeline} />
-        <comp.ForNPOs.ProjectCriteria
-          projectCriteria={projectCriteria}
-          testimonial={testimonial}
-        />
-        <comp.ForNPOs.ContactUs />
-        <section className="flex flex-col justify-center mb-16 px-8 lg:px-40">
-          <h1 className="text-4xl font-bold py-3">FAQs</h1>
-          <div className="w-full flex flex-col">
-            {faqs.map((faq, index) => (
-              <comp.DropdownFAQ faq={faq} index={index} key={index} />
-            ))}
-          </div>
-        </section>
-        <comp.Footer />
-      </>
+      <comp.Navbar />
+      <comp.Hero
+        title="Non-Profit Organizations"
+        description="We are passionate about leveraging our technical and consulting skills
+          to support your organization’s mission, free of charge!"
+        button1={{
+          text: "Email Us",
+          url: "mailto:sit.blueprint@gmail.com",
+        }}
+        button2={{ text: "See Our Projects", url: "/projects" }}
+        image={{
+          src: "/assets/vector/hands.svg",
+          alt: "hands",
+        }}
+      />
+      <comp.ForNPOs.ProjectTimeline projectTimeline={projectTimeline} />
+      <comp.ForNPOs.ProjectCriteria
+        projectCriteria={projectCriteria}
+        testimonial={testimonial}
+      />
+      <comp.ForNPOs.ContactUs />
+      <section className="flex flex-col justify-center mb-16 px-8 lg:px-40">
+        <h1 className="text-4xl font-bold py-3">FAQs</h1>
+        <div className="w-full flex flex-col">
+          {faqs.map((faq, index) => (
+            <comp.DropdownFAQ faq={faq} index={index} key={index} />
+          ))}
+        </div>
+      </section>
+      <comp.Footer />
     </body>
   </html>
 );
