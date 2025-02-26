d2ea00c2cdd3630daedd62c3e66cecbc7f4de68b
Caposto <christiand.apostol@outlook.com>
2025-02-26 10:01:28 -0500

    formatted cards for blog

--- src/_components/Blog/Gallery.jsx
+++ src/_components/Blog/Gallery.jsx
@@ -1,22 +1,28 @@
 export default function Gallery({ comp, posts }) {
   return (
-    <section className="flex flex-col items-center justify-center bg-gray-50">
+    <section className="flex flex-col items-center justify-center px-4 md:px-8 lg:px-28">
       {posts.map((post) => {
         return (
-          <div>
-            <h2>{post.title}</h2>
-            <div className="flex gap-1 content-center">
-              {post.tags.map((tag) => (
-                <div
-                  key={tag}
-                  className="bg-primary rounded-md px-3 text-sm font-semibold text-white text-center truncate"
-                >
-                  {tag}
-                </div>
-              ))}
+          <div className="flex flex-col bg-white p-4 m-4 rounded-md shadow-md">
+            <a href={post.redirect_url}>
+              <h2 className="text-3xl font-bold hover:text-primary hover:underline">
+                {post.title}
+              </h2>
+            </a>
+            <div className="flex">
+              <div className="flex gap-1 content-center">
+                {post.tags.map((tag) => (
+                  <div
+                    key={tag}
+                    className="bg-primary rounded-md px-3 text-sm font-semibold text-white text-center truncate"
+                  >
+                    {tag}
+                  </div>
+                ))}
+              </div>
+              <p>{post.date_posted}</p>
             </div>
-            <p>{post.date_posted}</p>
-            <p>{post.preview}</p>
+            <p className="text-xl">{post.preview}</p>
           </div>
         );
       })}
