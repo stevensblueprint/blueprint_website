aabf0ea39f407ada281361c8f53a28ddd4c4e9a0
Christian Apostol <christiand.apostol@outlook.com>
2025-03-02 19:47:45 -0500

    created basic partners hero

---
+++ src/_includes/_layouts/Partners.jsx
@@ -0,0 +1,34 @@
+export default ({ comp }) => (
+  <html>
+    <head>
+      <comp.OpenGraphCommon />
+      <link rel="stylesheet" href="/styles.css" />
+      <title>Partners</title>
+      <meta property="og:title" content="About Us" />
+      <meta property="og:url" content="https://sitblueprint.com/about/" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0"
+      ></meta>
+    </head>
+    <body>
+      <div>
+        <comp.Navbar />
+        <comp.Hero
+          title="Partners"
+          description="Whether through sponsorships, joint events, or mentorship opportunities, partnering with us means investing in the next generation of leaders. Let’s work together to create meaningful impact—reach out to us today!"
+          button1={{
+            text: "Partner With Us",
+            url: "mailto:sit.blueprint@gmail.com",
+          }}
+          button2={{ text: "See Our Projects", url: "/projects" }}
+          image={{
+            src: "/assets/vector/partnership_handshake.svg",
+            alt: "A graphic showing a handshake between two people",
+          }}
+        />
+        <comp.Footer />
+      </div>
+    </body>
+  </html>
+);
--- src/partners.md
+++ src/partners.md
@@ -1,6 +1,6 @@
 ---
 title: Stevens Blueprint
-layout: _layouts/Placeholder.jsx
+layout: _layouts/Partners.jsx
 headline: Under Construction.
 description: We appreciate your patience, come back soon!
 ---
