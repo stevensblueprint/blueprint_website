bb7e3d8ecb5ef14071c0e08c80fe2da59a0b4981
Christian Apostol <94158310+Caposto@users.noreply.github.com>
2025-04-10 15:08:06 -0400

    added width and height constraints and changed wording of sponsor list item to be more consistent (#374)

--- src/_components/Sponsors/ListItem.jsx
+++ src/_components/Sponsors/ListItem.jsx
@@ -1,7 +1,7 @@
 export default function ListItem({ item, iconPath }) {
   return (
     <div className="flex pt-8">
-      <img className="pr-3" src={iconPath} alt="icon" />
+      <img className="pr-3 h-8 w-10" src={iconPath} alt="icon" />
       <p className="text-xl">{item}</p>
     </div>
   );
