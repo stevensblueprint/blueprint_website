b277ddb8ece29672df4d0135dd3211a0bdbaf58d
# <me@rebound.sh>
2025-09-11 00:09:50 -0400

    style: lint

--- src/_components/Carousel.jsx
+++ src/_components/Carousel.jsx
@@ -1,30 +1,30 @@
 import React from "react";
 
 export default function Carousel({ array_of_images }) {
-	return (
-		<div className="w-full lg:max-h-[40rem] overflow-y-scroll relative">
-			<div className="flex gap-6 overflow-scroll lg:px-32 px-12 py-4 max-md:px-16 max-sm:flex-col max-sm:px-4">
-				<div className="absolute top-0 left-0 w-36 h-full bg-gradient-to-l from-transparent to-white max-sm:hidden"></div>
+  return (
+    <div className="w-full lg:max-h-[40rem] overflow-y-scroll relative">
+      <div className="flex gap-6 overflow-scroll lg:px-32 px-12 py-4 max-md:px-16 max-sm:flex-col max-sm:px-4">
+        <div className="absolute top-0 left-0 w-36 h-full bg-gradient-to-l from-transparent to-white max-sm:hidden"></div>
 
-				{array_of_images.map((image, i) => {
-					return (
-						<div
-							key={i}
-							className="flex justify-center shrink-0 lg:w-[35rem] w-[55rem] h-auto max-sm:w-full"
-						>
-							<img
-								className="rounded-lg"
-								src={image}
-								alt="carousel image"
-								loading="lazy"
-								sizes="(max-width: 768px) 100vw, (max-width: 1024px) 55rem, 35rem"
-							/>
-						</div>
-					);
-				})}
-			</div>
-			<div className="absolute top-0 right-0 w-36 h-full bg-gradient-to-r from-transparent to-white max-sm:hidden"></div>
-			{/* 
+        {array_of_images.map((image, i) => {
+          return (
+            <div
+              key={i}
+              className="flex justify-center shrink-0 lg:w-[35rem] w-[55rem] h-auto max-sm:w-full"
+            >
+              <img
+                className="rounded-lg"
+                src={image}
+                alt="carousel image"
+                loading="lazy"
+                sizes="(max-width: 768px) 100vw, (max-width: 1024px) 55rem, 35rem"
+              />
+            </div>
+          );
+        })}
+      </div>
+      <div className="absolute top-0 right-0 w-36 h-full bg-gradient-to-r from-transparent to-white max-sm:hidden"></div>
+      {/* 
       // Control Buttons
       <span className="absolute top-1/2 left-[5.5rem] -translate-y-1/2 z-10 h-1/5 aspect-square grid place-content-center shadow-xl bg-white rounded-xl max-sm:hidden">
         Placeholder
@@ -33,6 +33,6 @@ export default function Carousel({ array_of_images }) {
         Placeholder
       </span>
       */}
-		</div>
-	);
+    </div>
+  );
 }
--- src/_components/Hero.jsx
+++ src/_components/Hero.jsx
@@ -1,36 +1,36 @@
 import React from "react";
 
 export default function Hero({
-	comp,
-	title,
-	description,
-	button1,
-	button2,
-	image,
+  comp,
+  title,
+  description,
+  button1,
+  button2,
+  image,
 }) {
-	return (
-		<section className="grow flex space-between flex-col min-h-[600px] lg:flex-row lg:items-center relative overflow-hidden bg-primary text-white px-6 md:px-12 lg:px-32">
-			<div className="py-8 md:py-16 lg:py-0 w-full lg:pr-16">
-				<h1 className="font-bold text-3xl md:text-4xl lg:text-5xl leading-snug md:leading-[1.3] tracking-wide mb-4 md:mb-6 lg:mb-16">
-					{title}
-				</h1>
-				<p className="text-lg md:text-xl lg:text-2xl leading-snug md:leading-[1.3] mb-6 md:mb-10 lg:mb-14">
-					{description}
-				</p>
-				<div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-6">
-					<comp.HeroButton text={button1.text} redirect_url={button1.url} />
-					<comp.HeroButton text={button2.text} redirect_url={button2.url} />
-				</div>
-			</div>
-			<div className="flex sm:w-full lg:w-3/4 justify-center p-2 sm:p-16 lg:max-w-[600px] lg:max-h-[550px] mb-8">
-				<img
-					src={image.src}
-					alt={image.alt}
-					className={image.className ?? ""}
-					loading="lazy"
-					sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 600px"
-				/>
-			</div>
-		</section>
-	);
+  return (
+    <section className="grow flex space-between flex-col min-h-[600px] lg:flex-row lg:items-center relative overflow-hidden bg-primary text-white px-6 md:px-12 lg:px-32">
+      <div className="py-8 md:py-16 lg:py-0 w-full lg:pr-16">
+        <h1 className="font-bold text-3xl md:text-4xl lg:text-5xl leading-snug md:leading-[1.3] tracking-wide mb-4 md:mb-6 lg:mb-16">
+          {title}
+        </h1>
+        <p className="text-lg md:text-xl lg:text-2xl leading-snug md:leading-[1.3] mb-6 md:mb-10 lg:mb-14">
+          {description}
+        </p>
+        <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-6">
+          <comp.HeroButton text={button1.text} redirect_url={button1.url} />
+          <comp.HeroButton text={button2.text} redirect_url={button2.url} />
+        </div>
+      </div>
+      <div className="flex sm:w-full lg:w-3/4 justify-center p-2 sm:p-16 lg:max-w-[600px] lg:max-h-[550px] mb-8">
+        <img
+          src={image.src}
+          alt={image.alt}
+          className={image.className ?? ""}
+          loading="lazy"
+          sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 600px"
+        />
+      </div>
+    </section>
+  );
 }
